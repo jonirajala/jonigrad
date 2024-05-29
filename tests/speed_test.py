5585f288704899385cb659002f56763c5e4319a3
jonirajala <joni.m.rajala@gmail.com>
2024-05-29 14:08:30 +0300

    lstm to speedtest

--- tests/speed_test.py
+++ tests/speed_test.py
@@ -6,8 +6,17 @@ import torch.optim as optim
 import time
 import matplotlib.pyplot as plt
 
-from jonigrad.layers import Linear, ReLU, CrossEntropyLoss, Conv, Module, Flatten
-from jonigrad.utils import load_mnist
+from jonigrad.layers import (
+    Linear,
+    ReLU,
+    CrossEntropyLoss,
+    Conv,
+    Module,
+    Flatten,
+    LSTM,
+    MSELoss,
+)
+from jonigrad.utils import load_mnist, load_temperature_data
 
 BATCH_SIZE = 32
 ITERS = 100
@@ -16,12 +25,12 @@ g = np.random.default_rng()  # create a random generator
 
 comparison_template = """
 Performance Comparison
-----------------------------------------------------------------
-|     Layer      | Custom Time (s)      | Torch Time (s)       |
-|----------------|----------------------|----------------------|
-| Convolutional  | {:<20} | {:<20} |
-| MLP            | {:<20} | {:<20} |
-----------------------------------------------------------------
+-------------------------------------------------------------------------------------------
+|     Test        | Convolutional         | MLP                   | LSTM                  |
+|-----------------|-----------------------|-----------------------|-----------------------|
+| Custom Time (s) | {:<20}  | {:<20}  | {:<20}  |
+| Torch Time  (s) | {:<20}  | {:<20}  | {:<20}  |
+-------------------------------------------------------------------------------------------
 """
 
 
@@ -53,7 +62,7 @@ class ConvModel(Module):
         return dL_dx
 
 
-class MLP(Module):
+class MLPModel(Module):
     def __init__(self):
         self.fc1 = Linear(28 * 28, 256)
         self.relu = ReLU()
@@ -69,6 +78,25 @@ class MLP(Module):
         dL_dx = self.relu.backward(dL_dx)
         dL_dx = self.fc1.backward(dL_dx)
         return dL_dx
+
+
+class LSTMModel(Module):
+    def __init__(self, input_size, hidden_layer_size, output_size):
+        super().__init__()
+        self.hidden_layer_size = hidden_layer_size
+        self.lstm = LSTM(input_size, hidden_layer_size)
+        self.linear = Linear(hidden_layer_size, output_size)
+
+    def forward(self, input_seq):
+        lstm_out, _, _ = self.lstm.forward(input_seq)
+        predictions = self.linear.forward(lstm_out[:, -1])
+        return predictions
+
+    def backward(self, dL_dy):
+        dL_dy = self.linear.backward(dL_dy)
+        dL_dy = np.expand_dims(dL_dy, 2)
+        dl_dx = self.lstm.backward(dL_dy)
+        return dl_dx
 
 
 def custom_conv_test(Xb, Yb):
@@ -125,7 +153,7 @@ def torch_conv_test(Xb, Yb):
 
 def custom_mlp_test(Xb, Yb):
 
-    model = MLP()
+    model = MLPModel()
     loss_f = CrossEntropyLoss()
 
     start_time = time.time()
@@ -173,10 +201,64 @@ def torch_mlp_test(Xb, Yb):
     return end_time - start_time
 
 
+def custom_lstm_test(Xb, Yb):
+    model = LSTMModel(input_size=1, hidden_layer_size=50, output_size=1)
+    loss_function = MSELoss()
+
+    start_time = time.time()
+
+    model.train()
+    for iter in range(ITERS):
+        y_pred = model.forward(Xb)
+        loss = loss_function(y_pred, Yb)
+        model.zero_grad()
+        dL_dy = loss_function.backward()
+        model.backward(dL_dy)
+        model.step(LR)
+
+    end_time = time.time()
+
+    return end_time - start_time
+
+
+def torch_lstm_test(Xb, Yb):
+    class LSTMModelTorch(nn.Module):
+        def __init__(self, input_size, hidden_layer_size, output_size):
+            super(LSTMModelTorch, self).__init__()
+            self.hidden_layer_size = hidden_layer_size
+            self.lstm = nn.LSTM(input_size, hidden_layer_size, batch_first=True)
+            self.linear = nn.Linear(hidden_layer_size, output_size)
+
+        def forward(self, input_seq):
+            lstm_out, _ = self.lstm(input_seq)
+            predictions = self.linear(lstm_out[:, -1])
+            return predictions
+
+    model = LSTMModelTorch(input_size=1, hidden_layer_size=50, output_size=1)
+    loss_function = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=LR)
+
+    Xb = torch.tensor(Xb, dtype=torch.float32)
+    Yb = torch.tensor(Yb, dtype=torch.float32)
+
+    start_time = time.time()
+    model.train()
+    for iter in range(ITERS):
+        optimizer.zero_grad()
+        y_pred = model(Xb)
+        loss = loss_function(y_pred, Yb)
+        loss.backward()
+        optimizer.step()
+
+    end_time = time.time()
+
+    return end_time - start_time
+
+
 def main():
-    train_X, train_y, test_X, test_y = load_mnist(flatten=False)
-    ix = g.integers(low=0, high=train_X.shape[0], size=BATCH_SIZE)
-    Xb, Yb = train_X[ix], train_y[ix]
+    X_train, y_train, test_X, test_y = load_mnist(flatten=False)
+    ix = g.integers(low=0, high=X_train.shape[0], size=BATCH_SIZE)
+    Xb, Yb = X_train[ix], y_train[ix]
     print("Running Conv tests")
     custom_dt_conv = custom_conv_test(Xb, Yb)
     torch_dt_conv = torch_conv_test(Xb, Yb)
@@ -186,9 +268,24 @@ def main():
     custom_dt_mlp = custom_mlp_test(Xb, Yb)
     torch_dt_mlp = torch_mlp_test(Xb, Yb)
 
+    seq_length = 30
+    X_train, y_train, X_test, y_test, data, temperatures, scaler = (
+        load_temperature_data(seq_length)
+    )
+    ix = g.integers(low=0, high=X_train.shape[0], size=BATCH_SIZE)
+    Xb, Yb = X_train[ix], y_train[ix]
+    print("Running LSTM tests")
+    custom_dt_lstm = custom_lstm_test(Xb, Yb)
+    torch_dt_lstm = torch_lstm_test(Xb, Yb)
+
     print(
         comparison_template.format(
-            custom_dt_conv, torch_dt_conv, custom_dt_mlp, torch_dt_mlp
+            custom_dt_conv,
+            custom_dt_mlp,
+            custom_dt_lstm,
+            torch_dt_conv,
+            torch_dt_mlp,
+            torch_dt_lstm,
         )
     )
 
